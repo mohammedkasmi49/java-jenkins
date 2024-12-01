29086f70a9d358ba4d1da36cdd4533d65f1a4d4c
mohammedkasmi49 <127699685+mohammedkasmi49@users.noreply.github.com>
2024-12-01 01:14:39 +0100

    second commit

--- src/main/java/ma/nestle/myapp/service/MyService.java
+++ src/main/java/ma/nestle/myapp/service/MyService.java
@@ -2,7 +2,7 @@ package ma.nestle.myapp.service;
 
 public class MyService {
     public static double somme(double a,double b){
-        return a+b;//la somme de deux double
+        return a+b;
     }
     
 }
